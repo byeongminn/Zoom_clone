d8a226c6987151cb8d2e232c59c7d989d6892c77
unknown <sekn1997@naver.com>
2021-08-08 14:58:02 +0900

    IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -128,9 +128,24 @@ socket.on("answer", (answer) => {
     myPeerConnection.setRemoteDescription(answer);
 })
 
+socket.on("ice", (ice) => {
+    myPeerConnection.addIceCandidate(ice);
+})
+
 // RTC Code
 
 function makeConnection() {
     myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+    const peerFace = call.querySelector("#peerFace");
+    peerFace.srcObject = data.stream;
 }
